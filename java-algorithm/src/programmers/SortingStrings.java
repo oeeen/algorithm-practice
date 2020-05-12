824a6b868a29105c27b4c980561571d0d640afb6
Seongmo Jeon <oeeen3@gmail.com>
2020-05-12 16:28:09 +0900

    programmers: 문자열 내 마음대로 정렬하기 by java
    https://programmers.co.kr/learn/courses/30/lessons/12915

---
+++ java-algorithm/src/programmers/SortingStrings.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class SortingStrings {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (o1, o2) -> {
+            if (o1.charAt(n) == o2.charAt(n)) {
+                return o1.compareTo(o2);
+            } else {
+                return o1.charAt(n) - o2.charAt(n);
+            }
+        });
+        return strings;
+    }
+}
