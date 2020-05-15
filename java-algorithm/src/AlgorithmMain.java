549416767c9ba981ba1ab8049aba58cdea8fd0c6
Seongmo Jeon <oeeen3@gmail.com>
2020-05-15 16:47:35 +0900

    programmers: 주식 가격 by java
    https://programmers.co.kr/learn/courses/30/lessons/42584

--- java-algorithm/src/AlgorithmMain.java
+++ java-algorithm/src/AlgorithmMain.java
@@ -1,9 +1,9 @@
-import programmers.Tower;
+import programmers.StockPrice;
 
 public class AlgorithmMain {
     public static void main(String[] args) {
-        Tower sol = new Tower();
+        StockPrice sol = new StockPrice();
 
-        System.out.println(sol.solution(new int[]{6, 9, 5, 7, 4}));
+        System.out.println(sol.solution(new int[]{1, 2, 3, 2, 3}));
     }
 }
---
+++ java-algorithm/src/programmers/StockPrice.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class StockPrice {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[i] > prices[j]) {
+                    answer[i] = j - i;
+                    break;
+                }
+            }
+            if (answer[i] == 0) {
+                answer[i] = prices.length - i - 1;
+            }
+        }
+        return answer;
+    }
+}
