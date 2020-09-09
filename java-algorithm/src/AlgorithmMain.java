551c97534a253522492b32ed97976967a1465fdc
Seongmo Jeon <oeeen3@gmail.com>
2020-09-09 16:05:56 +0900

    leetcode: add two numbers

--- java-algorithm/src/AlgorithmMain.java
+++ java-algorithm/src/AlgorithmMain.java
@@ -1,9 +1,17 @@
-import leetcode.TwoSum;
+import leetcode.AddTwoNumbers;
 
 public class AlgorithmMain {
     public static void main(String[] args) {
-        TwoSum sol = new TwoSum();
+        AddTwoNumbers sol = new AddTwoNumbers();
 
-        System.out.println(sol.twoSum(new int[]{2, 7, 11, 15}, 9));
+
+        AddTwoNumbers.ListNode l1Third = new AddTwoNumbers.ListNode(3);
+        AddTwoNumbers.ListNode l1Second = new AddTwoNumbers.ListNode(4, l1Third);
+        AddTwoNumbers.ListNode l1First = new AddTwoNumbers.ListNode(2, l1Second);
+
+        AddTwoNumbers.ListNode l2Third = new AddTwoNumbers.ListNode(4);
+        AddTwoNumbers.ListNode l2Second = new AddTwoNumbers.ListNode(6, l2Third);
+        AddTwoNumbers.ListNode l2First = new AddTwoNumbers.ListNode(5, l2Second);
+        System.out.println(sol.addTwoNumbers(l1First, l2First));
     }
 }
---
+++ java-algorithm/src/leetcode/AddTwoNumbers.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode answer = new ListNode();
+        ListNode curr = answer;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+
+        if (carry == 1) {
+            curr.next = new ListNode(1);
+        }
+
+        return answer.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
